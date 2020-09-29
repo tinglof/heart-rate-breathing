54b7c6f0e16233968c37f2277e061b77a8f676e7
Johan Tinglöf <johan.tinglof@rebelandbird.com>
2020-09-29 22:16:53 +0200

    Added 3 minute option by request

--- src/components/page-components/hrv-breathing/hrv-breathing.js
+++ src/components/page-components/hrv-breathing/hrv-breathing.js
@@ -6,7 +6,7 @@ import NoSleep from 'external-packages/NoSleep.js';
 
 const IN_BREATH_VALUES = [4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 9, 10];
 const OUT_BREATH_VALUES = [5, 5.5, 6, 6.5, 7, 7.5, 8, 9, 10, 11, 12, 13, 14];
-const SESSION_LENGTH_VALUES = ['off', 5, 10, 15, 20, 25, 30, 35, 40, 45];
+const SESSION_LENGTH_VALUES = ['off', 3, 5, 10, 15, 20, 25, 30, 35, 40, 45];
 
 export default class HrvBreathing extends Component {
 
